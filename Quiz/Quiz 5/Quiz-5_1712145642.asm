0a6a18a713827d2b99b6c9226e75bb6852274add
MohammadFahad07 <fahad.mojumder@northsouth.edu>
2020-09-01 09:12:51 +0600

    Quiz 5 late submission due to save issue

--- Quiz/Quiz 5/Quiz-5_1712145642.asm
+++ Quiz/Quiz 5/Quiz-5_1712145642.asm
@@ -1,13 +1,58 @@
+.MODEL SMALL
+.STACK 100H
 
-; You may customize this and other start-up templates; 
-; The location of this template is c:\emu8086\inc\0_com_template.txt
+.DATA
+    PROMPT_1 DB 'Enter an integer less than 9: $'
+    PROMPT_2 DB 0AH, 0DH, 'The number is : $'
 
-org 100h
+.CODE
+    MAIN PROC
+        MOV AX, @DATA
+        MOV DS, AX
+        
+        LEA DX, PROMPT_1
+        MOV AH, 09H
+        INT 21H 
+        
+        MOV AH, 01H 
+        INT 21H
+        SUB AL, 30H
+        
+        MOV CL, 02H
+        
+        MOV AH, 00H
+        MOV BX, AX
 
-; add your code here
+@LOOP: DIV CL
+       CMP AH, 0H
+       JE @NOTX
+       INC CL
+       MOV AX, BX
+       CMP CL, BL
+       JL @LOOP
 
-ret
+       
+       LEA DX, PROMPT_2
+       MOV AH, 09H
+       INT 21H
+       MOV AH, 02
+       MOV DL, 'X'
+       JMP @END  
+       
 
-
-
-
+@NOTX: LEA DX, PROMPT_2
+       MOV AH, 09
+       INT 21H
+       MOV AH, 02H
+       MOV DL, 'X'
+       INT 21H
+       MOV DL, "'"
+       INT 21H
+       
+@END: HLT        
+        
+        
+    MAIN ENDP
+END MAIN
+        
+    
